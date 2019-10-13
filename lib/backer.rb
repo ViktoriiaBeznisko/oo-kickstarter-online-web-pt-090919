2cae3e436cc981a2b1fb691ce3bf6f42a4ae4541
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-13 17:57:18 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,9 +7,9 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(projects)
-    @backed_projects << projects
-    Project.backers = self
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
   end
     
 
